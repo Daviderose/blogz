368c12d1f2e12ff6905619598812394746005aa1
David Rose <fullbloomdrose@yahoo.com>
2018-03-27 12:21:47 -0500

    seperated main blog into new_post and added validation

--- main.py
+++ main.py
@@ -19,22 +19,43 @@ class Blog(db.Model):
 		self.title = title
 		self.body = body
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/blog', methods=['POST', 'GET'])
 def index():
 	
 	'''
 	owner = User.query.filter_by(email = session['email']).first()
 	'''
-	if request.method == 'POST':
-		blog_title = request.form['blog_title']
-		blog_text = request.form['blog_text']
-		new_blog = Blog(blog_title, blog_text) 
-		db.session.add(new_blog)
-		db.session.commit() 
 
 	blogs = Blog.query.all()
 	'''completed_tasks = Task.query.filter_by(completed=True, owner=owner).all()'''
-	return render_template('main_blog.html', title="Main Blog", blogs=blogs)
+	return render_template('main_blog.html', title="Build A Blog", blogs=blogs)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+
+	if request.method == 'POST':
+		blog_title = request.form['blog_title']
+		blog_text = request.form['blog_text']
+
+		empty_title_error = 'Please fill out the title'
+		empty_text_error = 'Please fill out the body'
+
+		if len(blog_title) > 0:
+			empty_title_error = ''
+
+		if len(blog_text) > 0:
+			empty_text_error = ''
+
+		if not empty_title_error and not empty_text_error:
+			new_post = Blog(blog_title, blog_text) 
+			db.session.add(new_post)
+			db.session.commit()
+			return redirect('/blog')
+		else:
+			return render_template('new_post.html', empty_title_error=empty_title_error, empty_text_error=empty_text_error, 
+				blog_title=blog_title, blog_text=blog_text )
+
+	return render_template('new_post.html', title="Add A Blog Entry")
 
 if __name__ == '__main__':
 	app.run()
